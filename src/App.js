405de5fafa3a1e7e51b79f0031db4b054645fede
TuanNH <tuana2k38@gmail.com>
2024-01-24 22:35:59 +0700

    cấu hình route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+
 function App() {
     return (
-        <div className="App">
-            <div>Huy Tuan</div>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+];
+
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
